fb8686a67b1a2c9e8b6fc9a6e56780f51c7ae3e7
Jorge09829985 <jdv.escobar@yavirac.edu.ec>
2025-11-17 07:32:59 -0500

    clase_15_d5

--- src/app/admin/inventario/components/categoria/categoria.component.ts
+++ src/app/admin/inventario/components/categoria/categoria.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CategoriaService } from '../../services/categoria.service';
+
+
+interface categoria{
+  id?: number,
+  nombre: string;
+  detalle?: string
+}
 
 @Component({
   selector: 'app-categoria',
   templateUrl: './categoria.component.html',
   styleUrl: './categoria.component.css'
 })
-export class CategoriaComponent {
+export class CategoriaComponent implements OnInit {
+
+  private categoriaService = inject(CategoriaService)
+
+  categorias: categoria[]=[]
+
+  ngOnInit(): void {
+      this.getCategorias()
+  }
+
+  getCategorias(){
+    
+  }
 
 }
